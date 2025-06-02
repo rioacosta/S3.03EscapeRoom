da26d336eee2ad21c34621eba0d69ad1a2b8a50a
Rio <acosta.hierba@gmail.com>
2025-06-02 17:17:52 +0200

    MySQL models completed

--- src/main/java/dao/mysqlimp/MySQLPlayerDAO.java
+++ src/main/java/dao/mysqlimp/MySQLPlayerDAO.java
@@ -2,8 +2,6 @@ package dao.mysqlimp;
 
 import dao.DatabaseConnection;
 import dao.interfaces.IGenericDAO;
-import model.Player;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
