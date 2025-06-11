21f1adb363d954ef9ae70c35f826c5fd2c2427c9
FlavioKde <149841602+FlavioKde@users.noreply.github.com>
2025-06-11 12:25:08 +0200

    final

--- src/main/java/com/escapeRoom/entities/RoomInputCollector.java
+++ src/main/java/com/escapeRoom/entities/RoomInputCollector.java
@@ -18,124 +18,8 @@ import java.util.Scanner;
             this.scanner = scanner;
             this.menuManager = menuManager;
         }
-     /*
-
-    public Room CollectNewRoomData() {
-        System.out.println("=====CREAR NUEVA SALA=====");
-
-        System.out.println("ID del escapeRoom");
-        int idEscaperoom_ref = menuManager.getValidatedIntegerInput();
-
-     //   String name = menuManager.collectNonEmptyLine("Nombre de la sala:");
-
-//        System.out.println("Nombre de la sala");
- //       String name = scanner.nextLine();
-
-        String name = "";
-        while (name.isBlank()) {
-            System.out.print("Ingrese un nombre: ");
-            name = scanner.nextLine();
-            if (name.isBlank()) {
-                System.out.println("El nombre no puede estar vacío.");
-            }
-        }
 
 
-        BigDecimal price;
-        while (true) {
-            try {
-                scanner.nextLine();
-                System.out.print("Precio base: ");
-                String input = scanner.nextLine().trim();
-
-                if (input.isEmpty()) {
-                    throw new IllegalArgumentException("El precio no puede estar vacío.");
-                }
-
-
-                input = input.replace(",", ".");
-
-                price = new BigDecimal(input);
-
-                if (price.compareTo(BigDecimal.ZERO) <= 0) {
-                    throw new IllegalArgumentException("El precio debe ser mayor a 0.");
-                }
-
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Formato inválido. Usá números como 12.5");
-            } catch (IllegalArgumentException e) {
-                System.out.println("  " + e.getMessage());
-            }
-        }
-
-
-
-        Difficulty difficulty;
-        do {
-            difficulty = menuManager.selectDifficulty();
-            if (difficulty == null) {
-                System.out.println("Seleccione una dificultad válida");
-            }
-        } while (difficulty == null);
-
-
-        Theme theme;
-        do{
-            theme = menuManager.selectTheme();
-            if (theme == null){
-                System.out.println("Selecciona un tema de sala valido");
-            }
-        } while (theme == null);
-
-        List<Hint> hintsList = new ArrayList<>();
-        System.out.print("¿Cuántas pistas querés agregar?: ");
-        int cantidadHints = scanner.nextInt();
-        scanner.nextLine();
-
-        for (int i = 0; i < cantidadHints; i++) {
-            System.out.print("Descripción de la pista #" + (i + 1) + ": ");
-            String desc = scanner.nextLine();
-
-
-            System.out.print("Precio de la pista: ");
-            BigDecimal hintPrice = new BigDecimal(scanner.nextLine());
-
-            hintsList.add(new Hint(0, 0, desc, theme, hintPrice));
-        }
-
-        List<Decoration> decorations = new ArrayList<>();
-        System.out.print("¿Cuántas decoraciones querés agregar?: ");
-        int cantidadDecoraciones = scanner.nextInt();
-        scanner.nextLine();
-
-        for (int i = 0; i < cantidadDecoraciones; i++) {
-            System.out.print("Descripción de la decoración #" + (i + 1) + ": ");
-            String desc = scanner.nextLine();
-
-            System.out.println("Que tipo de material tiene la decoración?");
-            String deco = scanner.nextLine();
-
-            System.out.print("Precio de la decoración: ");
-            BigDecimal decoPrice = new BigDecimal(scanner.nextLine());
-
-
-            decorations.add(new Decoration(desc, deco, decoPrice));
-        }
-        return new ConcreteBuilder()
-                .setIdEscaperoom_ref(idEscaperoom_ref)
-                .setName(name)
-                .setDifficulty(difficulty)
-                .setPrice(price)
-                .setTheme(theme)
-                .setHints(hintsList)
-                .setDecorations(decorations)
-                .build();
-    }
-
-      */
-
-       //prueba
 
 
 
@@ -143,7 +27,7 @@ import java.util.Scanner;
             System.out.println("===== CREAR NUEVA SALA =====");
 
             System.out.println("ID del escapeRoom");
-            int idEscapeRoomRef = menuManager.getValidatedIntegerInput(); // No necesita limpieza después
+            int idEscapeRoomRef = menuManager.getValidatedIntegerInput();
 
             String name = collectRoomName();
             BigDecimal price = collectRoomPrice();
@@ -170,7 +54,7 @@ import java.util.Scanner;
                 System.out.print("Nombre de la sala: ");
                 name = scanner.nextLine();
                 if (name.isBlank()) {
-                    System.out.println("⚠️ El nombre no puede estar vacío.");
+                    System.out.println("El nombre no puede estar vacío.");
                 }
             }
             return name;
@@ -233,23 +117,6 @@ import java.util.Scanner;
 
         return roomId;
     }
-/*
-    public int collectFieldToUpdate() {
-        int updateOption;
-
-        do {
-            updateOption = menuManager.showUpdateMenu();
-            if (updateOption != 0) {
-                System.out.println("Selecciona un campo para modificar");
-            }
-        } while (updateOption == 0);
-
-        return updateOption;
-    }
-
- */
-
-        //prueba
 
         public int collectFieldToUpdate() {
             while (true) {
@@ -267,10 +134,6 @@ import java.util.Scanner;
                 System.out.println("Opción inválida. Por favor seleccione 1-4 o 0 para salir");
             }
         }
-
-
-
-
 
 
     public String askForNewName(){
